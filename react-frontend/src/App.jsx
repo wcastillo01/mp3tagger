aacb333ff165ba652177f69c54f106d7e2628644
Wilson Castillo <wcastillo1@MacBook-Pro-de-Wilson-2.local>
2024-11-06 23:23:14 -0400

    centralize .gitignore

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,25 +1,8 @@
-import { useState } from 'react'
-import axios from 'axios';
+// import { useState } from 'react'
+// import axios from 'axios';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-  
-    try {
-      const response = await axios.post('http://127.0.0.1:5000/', {
-        file_path: filePath,
-        tags: tags
-      });
-      console.log(response.data);
-    } catch (error) {
-      console.error('Error updating tags:', error);
-    }
-  };
-
-
   return (
     <>
       <h1>MP3 TAGGER</h1>
